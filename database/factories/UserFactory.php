1dd4779938dd8b151af786b013643aec6f629296
visal <sokchamraeunid@gmail.com>
2026-05-13 22:45:16 +0700

    fix faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,8 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+ 
 
 /**
  * @extends Factory<User>
@@ -25,11 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+
         ];
     }
 
