c7e02cee044a477aa2d4eed200e770fa6452446f
visal <sokchamraeunid@gmail.com>
2026-05-13 23:27:13 +0700

    remove QR

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\PaymentController;
+// use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\CheckoutController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::prefix('payment')->group(function () {
-    Route::get('/', [PaymentController::class, 'index'])->name('payment.index');
-    Route::post('/generate', [PaymentController::class, 'generate'])->name('payment.generate');
-    Route::get('/success/{id}', [PaymentController::class, 'success'])->name('payment.success');
-    Route::get('/verify', [PaymentController::class, 'verify'])->name('payment.verify');
-    Route::post('/verify', [PaymentController::class, 'verifyCheck'])->name('payment.verify.check');
-});
+// Route::prefix('payment')->group(function () {
+//     Route::get('/', [PaymentController::class, 'index'])->name('payment.index');
+//     Route::post('/generate', [PaymentController::class, 'generate'])->name('payment.generate');
+//     Route::get('/success/{id}', [PaymentController::class, 'success'])->name('payment.success');
+//     Route::get('/verify', [PaymentController::class, 'verify'])->name('payment.verify');
+//     Route::post('/verify', [PaymentController::class, 'verifyCheck'])->name('payment.verify.check');
+// });
 
 Route::get('/', [ProductsController::class, 'index'])->name('products.index');
 Route::get('/products/{product}', [ProductsController::class, 'show'])->name('products.show');
